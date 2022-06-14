8040c62c08cbe40aaeafb7c377c9df02d2bcf7ae
datlongboi <74936472+datlongboi@users.noreply.github.com>
2022-06-14 09:34:32 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,37 @@
 package com.trashthing;
 
-import java.util.Scanner;
-import java.util.Random;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 
 public class Main {
     //i hate comments
     public static void main(String[] args) {
+
+        int playerLevel = 1;
+        String playerName;
+        double playerHealth = 10.0;
+        String randomWord;
+
+        Scanner Pinput = new Scanner(System.in);
         Random generator = new Random();
+        TimeUnit time = TimeUnit.valueOf("SECONDS");
+
+        System.out.println("Please choose a name for your character");
+        List<String> exclamations = Arrays.asList("admirable", "amazing", "astonishing", "awesome", "brilliant", "cool", "excellent", "fabulous", "fantastic", "incredible", "magnificent", "marvelous", "outstanding", "phenomenal", "remarkable", "superb", "surprising", "terrific", "tremendous", "wondrous", "astounding", "awe-inspiring", "divine", "dynamite", "groovy", "miraculous", "staggering", "startling", "stupendous", "super", "swell", "unheard-of");
+        Random rand = new Random();
+        playerName = Pinput.nextLine() + " the " + exclamations.get(rand.nextInt(exclamations.size()-1));
+        System.out.println("The " + exclamations.get(rand.nextInt(exclamations.size())) + " tale of " + playerName + " begins today!");
+
+        public static void waitFor (TimeUnit time,int x){
+            try {
+                time.SECONDS.sleep((ling) x);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        /*Random generator = new Random();
         Scanner Pinput;
         String PInput;
         int moneyzzzz;
@@ -16,6 +40,6 @@ public class Main {
         if (PInput.contains("money")) {
             moneyzzzz = generator.nextInt(500000);
             System.out.println("You stumble upon the thieves secret cache. You find " + moneyzzzz + " dollars!");
-        }
+        }*/
     }
 }
