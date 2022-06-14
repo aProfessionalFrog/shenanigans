850d53c0b45815fce2c70dd4c53e9c0fac8f3488
datlongboi <74936472+datlongboi@users.noreply.github.com>
2022-06-14 13:58:09 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,35 +1,59 @@
 package com.trashthing;
 
+import java.sql.Time;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 
 public class Main {
     //i hate comments
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         int playerLevel = 1;
         String playerName;
         double playerHealth = 10.0;
         String randomWord;
+        boolean invalid = true;
 
         Scanner Pinput = new Scanner(System.in);
         Random generator = new Random();
-        TimeUnit time = TimeUnit.valueOf("SECONDS");
+
 
         System.out.println("Please choose a name for your character");
         List<String> exclamations = Arrays.asList("admirable", "amazing", "astonishing", "awesome", "brilliant", "cool", "excellent", "fabulous", "fantastic", "incredible", "magnificent", "marvelous", "outstanding", "phenomenal", "remarkable", "superb", "surprising", "terrific", "tremendous", "wondrous", "astounding", "awe-inspiring", "divine", "dynamite", "groovy", "miraculous", "staggering", "startling", "stupendous", "super", "swell", "unheard-of");
         Random rand = new Random();
-        playerName = Pinput.nextLine() + " the " + exclamations.get(rand.nextInt(exclamations.size()-1));
+        playerName = Pinput.nextLine() + " the " + exclamations.get(rand.nextInt(exclamations.size() - 1));
         System.out.println("The " + exclamations.get(rand.nextInt(exclamations.size())) + " tale of " + playerName + " begins today!");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("You approach the mouth of a cave.");
+        System.out.println("Do you dare to enter? y/n");
+        while (invalid) {
+            if (Pinput.nextLine().toLowerCase().contains("y")) {
+                System.out.println("You enter the cave fearlessly. However, you made the wrong decision.\n Just seconds after you entered the cave, a bear cub came out and attacked you.");
+                invalid = false;
+                double damage = ((double) Math.round((generator.nextDouble() * 2 + 1) * 10)) / 10;
+                playerHealth -= damage;
+                    TimeUnit.MILLISECONDS.sleep(100);
+                System.out.print("You took " + damage + " damage.\n [");
+                int roundedHealth = (int) Math.round(playerHealth);
+                for (int i = 0; i < 10; i++) {
+                    if (i < roundedHealth) {
+                        System.out.print("♡");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.print("] " + playerHealth + "/10");
 
-        public static void waitFor (TimeUnit time,int x){
-            try {
-                time.SECONDS.sleep((ling) x);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            } else if (Pinput.nextLine().toLowerCase().contains("n")) {
+                System.out.println("");
+                invalid = false;
+            } else {
+                System.out.println("That is not an option");
             }
         }
+        invalid = true;
+
 
         /*Random generator = new Random();
         Scanner Pinput;
