d97b4df492eaf2176bc05c981cde62d91facb796
datlongboi <74936472+datlongboi@users.noreply.github.com>
2022-06-15 09:14:42 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,40 @@
 package com.trashthing;
 
-import java.util.Scanner;
 import java.util.Random;
-
+import java.util.Scanner;
 
 public class Main {
-    //i hate comments
+
     public static void main(String[] args) {
+        Scanner PInput = new Scanner(System.in);
         Random generator = new Random();
-        Scanner Pinput;
-        String PInput;
-        int moneyzzzz;
-        Pinput = new Scanner(System.in);
-        PInput = Pinput.nextLine();
-        if (PInput.contains("money")) {
-            moneyzzzz = generator.nextInt(500000);
-            System.out.println("You stumble upon the thieves secret cache. You find " + moneyzzzz + " dollars!");
+        boolean wrong = true;
+        int guess;
+        int guesses;
+        while (true) {
+            System.out.println("Choose a max number!");
+            int maxnumber = Math.round(Integer.parseInt(PInput.nextLine()));
+            System.out.println("Guess a number from 1 to " + maxnumber);
+            int number = generator.nextInt(maxnumber + 1);
+            wrong = true;
+            guesses = 0;
+            while (wrong) {
+                guess = Math.round(Integer.parseInt(PInput.nextLine()));
+                guesses++;
+                if (guess > maxnumber) {
+                    System.out.println("That is above the maximum number!");
+                } else if (guess > number) {
+                    System.out.println("Lower");
+                } else if (guess < number) {
+                    System.out.println("Higher");
+                } else if (guess == number) {
+                    System.out.println("🎉You got it in " + guesses + " guesses!🎉");
+                    wrong = false;
+                } else {
+                    System.out.println("Something went wrong.");
+                    guesses--;
+                }
+            }
         }
     }
 }
