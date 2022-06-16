990b5eb5e4ee94032684ab5f02f69bc80246c86f
Sirdug <80724707+Sirdug@users.noreply.github.com>
2022-06-16 13:07:56 -0700

    has enemys or thing that end the game.
    kskjdfhkjksdhfhslkajhfslafdkjh

--- Main.java
+++ Main.java
@@ -1,151 +1,157 @@
-package com.company;
+package project;
+
 import processing.core.PApplet;
-import processing.core.PSurface;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.lang.Float.valueOf;
-//https://www.msn.com/en-us/news/us/an-elementary-school-thought-it-was-serving-milk-to-students-turns-out-it-was-floor-sealant/ar-AAYxAs1?ocid=winp1taskbar&cvid=57c2fe01f84d444481a94afae70264e6
-public class Main extends PApplet {
 
-    int frameX;
-    int frameY;
-    int LframeY;
-    int maxX = 600;
-    int maxY = 400;
-    double playerX = maxX / 2;
-    double LplayerY;
-    double playerY = maxY / 2;
-    boolean left, right, up, down, draggable = false, firstFrame = true, wCanBeDown = true,firstHit=true;
-    double playerYVelocity;
+ //// This Is The Setup For The Window And Game \\\\
+public class project extends PApplet {
+
+    public int playerX = 40;
+    public int GRAVITY = 1;
+    public int TERMINAL_VELOCITY = 4;
+    public int vertical_speed = 0;
+
+    public int playerY = 40;
+    boolean left, right, up, down;
+    public boolean pressable;
+    public int delay = 0;
+    public List<Obstacle> enemyList = new ArrayList<>();
+    public int enemyDelay = 50;
+    public int count = 0;
+    public Obstacle jon;
     int gridHeight=30;
 
     public static void main(String[] args) {
-        PApplet.main("com.company.Main");
-
+        PApplet.main("project.project");
     }
-
     public void settings() {
-        size(maxX, maxY);
+        size(500, 200);
     }
 
     public void setup() {
-
+        pressable = true;
+        jon = new Obstacle(500, 150, "rectangle",4);
+        enemyList.add(jon);
     }
 
+     //// This Is The Gravity For The Game \\\\
+    public void fall () {
+            vertical_speed = vertical_speed + GRAVITY;
+            if (vertical_speed > TERMINAL_VELOCITY)
+            {
+                vertical_speed = TERMINAL_VELOCITY;
+            }
+            playerY = playerY + vertical_speed;
+    }
 
     public void draw() {
         background(0);
         noStroke();
         drawPlayer();
-        if (firstFrame) {
-            firstFrame = false;
-            frameX = getJFrame(getSurface()).getX();
-            frameY = getJFrame(getSurface()).getY();
+        fall();
+        fps();
+        checkcolision();
+        for (Obstacle check : enemyList) {
+            check.update();
+            fill(255, 255, 255);
+            rect(check.xLoc, check.yLoc, check.xsize, check.ysize);
+        }
+        if (count == enemyDelay){
+            count = 0;
+            enemyList.add(new Obstacle(500, (int)(Math.random()*height), "rectangle", (int)(Math.random()*5)+1));
         } else {
-            if (draggable) {
-                frameX = getJFrame(getSurface()).getX();
-                frameY = getJFrame(getSurface()).getY();
-                playerY = Math.round(maxY / 2);
-            } else {
-                surface.setLocation(frameX, frameY);
+            count++;
+        }
+        if (!pressable){
+            delay++;
+            if (delay == 20){
+                delay = 0; // THIS IS WHAT RAPTOR ADDED
+                pressable = true;
             }
         }
-        text(String.valueOf((float)Math.round(valueOf(frameRate)*10)/10),4f , 14f);
+      ///// This Is The Fps Counter Code \\\\
     }
 
-    static final javax.swing.JFrame getJFrame(final PSurface surface) {
-        return (javax.swing.JFrame) ((processing.awt.PSurfaceAWT.SmoothCanvas) surface.getNative()).getFrame();
+     public void fps() {
+        fill(0, 230, 50);
+        text(String.valueOf(Math.round(frameRate)),7f , 11f);
     }
 
+    //// collision and physics \\\\
+     public void checkcolision() {
+         for (Obstacle check : enemyList) {
+             if (playerX + 10 > check.xLoc && playerX + 10 < check.xLoc + check.xsize && playerX + 10 > check.xLoc && playerX + 10 < check.xLoc + check.xsize) {
+                 //// take away + 10 for x\\\\
+                 if (playerY > check.yLoc && playerY < check.yLoc + check.ysize && playerY + 10 > check.yLoc && playerY + 10 < check.yLoc + check.ysize) {
+                     System.out.println("CollidedY");
+                     exit();
+                 }
+             }
+         }
+     }
 
+    //// PLayer Mechanics And the Color And Fill \\\\
     public void drawPlayer() {
-        playerYVelocity -= 0.4598364923;
-        //window movement
-        if(LframeY==frameY){
-            frameX = getJFrame(getSurface()).getX();
-            frameY = getJFrame(getSurface()).getY();
-        }
+       playerY++;
 
-        if(playerX>= maxX-0.001) {
-        frameX+=maxX-5;
-        playerX =1;
+        if (up) {
+            playerY -= 11;
         }
-        if(playerX<= 0.001) {
-            frameX-=maxX-5;
-            playerX =maxX;
-        }
-        if (playerY >= maxY - 0.001) {
-            playerYVelocity = 0;
-        }
-        if (playerY <= 0.001) {
-            playerYVelocity = -0.1;
-            playerY += 1;
-        }
-        if (playerY==LplayerY){
-            playerYVelocity=0;
-        }
-        LplayerY=playerY;
-        playerY -= playerYVelocity;
         if (left) {
-                playerX -= 5;
-            if (playerX >= 0.001 && playerX <= maxX - 0.001) {
-                frameX += 5;
-            }
+            playerX -= 3;
         }
         if (right) {
-                playerX += 5;
-            if (playerX >= 0.001 && playerX <= maxX - 0.001) {
-                frameX -= 5;
-            }
+            playerX += 3;
         }
         if (down) {
-            if (playerY >= 0.001 && playerY <= maxY - 0.001) {
-                playerY += 5;
-            }
+            playerY += 3;
         }
-
-        LframeY=frameY;
-
-        fill(0, 230, 172);
+        if (playerY > height - 5) {
+            playerY = height -5;
+        }
+        fill(0, 530, 550);
         rectMode(CENTER);
-        playerX = constrain((float) playerX, 0, width);
-        playerY = constrain((float) playerY, 0, height-gridHeight);
-        rect((float) playerX, (float) playerY, (gridHeight/3)*2, (gridHeight/3)*2);
+        playerX = constrain(playerX, 0, width);
+        playerY = constrain(playerY, 0, height);
+        rect(playerX, playerY, 10, 10);
+        fill(0, 0, 0);
+        rect(playerX, playerY, 5, 5);
     }
 
     public void keyPressed() {
-        if (key == 'w' || key == 'W' || key == 32) {
-                if (playerY==LplayerY) {
-                    playerY -= 1.2;
-                    playerYVelocity += 11;
-                    //up = true;
-            }
+        if (key == 'w') {
+            up = true;
         }
-        if (key == 'a' || key == 'A') {
+        if (key == 'a') {
             left = true;
         }
-        if (key == 's' || key == 'S') {
+        if (key == 's') {
             down = true;
         }
-        if (key == 'd' || key == 'D') {
+        if (key == 'd') {
             right = true;
         }
-        if (key == 'p' || key == 'P') {
-            draggable = !draggable;
-            firstHit=true;
+        if (key == ' ' && pressable) {
+            pressable = false;
+            vertical_speed = -14;
         }
     }
 
     public void keyReleased() {
-        if (key == 'w' || key == 'W' || key == 32) {
-            //up = false;
+        if (key == 'w') {
+            up = false;
         }
-        if (key == 'a' || key == 'A') {
+        if (key == 'a') {
             left = false;
         }
-        if (key == 's' || key == 'S') {
+        if (key == 's') {
             down = false;
         }
-        if (key == 'd' || key == 'D') {
+        if (key == 'd') {
             right = false;
         }
     }
